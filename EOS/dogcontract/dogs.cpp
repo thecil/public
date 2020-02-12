55d10c262d0e42e9c92b6cc09bcb2c763e155e69
thecil <53628989+thecil@users.noreply.github.com>
2020-02-12 19:39:36 -0400

    Update dogs.cpp

--- EOS/dogcontract/dogs.cpp
+++ EOS/dogcontract/dogs.cpp
@@ -7,45 +7,102 @@ using namespace eosio;
 
 #include "table/user/dog.hpp"
 
+
 CONTRACT dogs : public contract {
   public:
     using contract::contract;
     dogs(name receiver, name code, datastream<const char*> ds)
     :contract(receiver, code, ds){}
 
-  ACTION insdog(name from, std::string dog_name, std::string dog_img, uint8_t dog_age) {
-    require_auth(from);
-
-
-    // Init the _message table
+  /*
+  ** Function insert data into table by ID.
+  ** Owner can not be changed in this function.
+  */
+  ACTION insdog(name owner, std::string dog_name, std::string dog_img, uint8_t dog_age) {
+    //Require auth of the owner
+    require_auth(owner);
+    //Get the index of our table
     dog_index dogtable(get_self(), get_self().value); //this is an scope);
-
-      dogtable.emplace(from, [&](auto& target) {
-        target.id = dogtable.available_primary_key();
-        target.owner = from;
-        target.rows.push_back({
-          dog_name,
-          dog_img,
-          dog_age
-        });
+    //Execute the insert function. Specifying the dog_name,age and img link,
+    //payer of storage and a lambda function
+    dogtable.emplace(owner, [&](auto& target) {
+      target.id = dogtable.available_primary_key();
+      target.owner = owner;
+      //create a dogrow and set and the end(push_back) values
+      target.rows.push_back({
+        dog_name,
+        dog_img,
+        dog_age
       });
+    });
 
-  }
+  }//end ACTION insdog
 
-  ACTION rmdog() {
-    require_auth(get_self());
-
+  /*
+  ** Function modifies the name and age of a dog with the dog_id.
+  ** Owner can not be changed in this function.
+  */
+  ACTION moddog(int dog_id, std::string dog_name, std::string dog_img, uint8_t dog_age){
+    //Get the index of our table
     dog_index dogtable(get_self(), get_self().value);
+    //Fetch the current data of our dog
+    auto dog = dogtable.get(dog_id, "Unable ot fetch dog.");
+    //Require auth of the owner
+    require_auth(dog.owner);
+    //Get the iterator to be able to find and modify the row in the table
+    auto iterator = dogtable.find(dog_id);
+    //Execute the modify function. Specifying the iterator,
+    //payer of storage and a lambda function.
+    dogtable.modify(iterator, dog.owner, [&](auto& target){
+      //clear vector before new data
+      target.rows.clear();
+      //insert new data
+      target.rows.push_back({
+        dog_name,
+        dog_img,
+        dog_age
+      });
+      
+    });
+  }//end ACTION modify
+  
+  /*
+  ** Function delete dog by dog_id.
+  ** Owner can not be changed in this function.
+  */
+  ACTION rmdog (int dog_id) {
+    //Get the index of our table
+    dog_index dogtable(get_self(), get_self().value); //this is an scope
+    //Fetch the current data of our dog
+    auto dog = dogtable.get(dog_id, "Unable to fetch dog.");
+    //Require auth of the owner
+    require_auth(dog.owner);
+    //Get the iterator to be able to find the row in the table
+    auto iterator = dogtable.find(dog_id);
+    //Execute the erase function. Specifying the iterator,
+    dogtable.erase(iterator);
+  }//end ACTION erase
 
-    // Delete all records in _messages table
-    auto dog_itr = dogtable.begin();
-    while (dog_itr != dogtable.end()) {
-      dog_itr = dogtable.erase(dog_itr);
+  /*
+  ** Function remove all dogs by Owner.
+  ** Owner can not be changed in this function.
+  */
+  ACTION rmadog(name user) {
+    //Get the index of our table
+    dog_index dogtable(get_self(), get_self().value);
+    //Fetch the current data of dogs by owner
+    auto owner_index = dogtable.get_index<"byowner"_n>();
+    //Get the iterator to be able to find the row in the table
+    auto iterator = owner_index.find(user.value);
+    while(iterator != owner_index.end()){
+      //delete row
+      owner_index.erase(iterator);
+      iterator = owner_index.find(user.value);
     }
-  }
+  }//end ACTION rmadog
 
   ACTION getdog(name from){
-  // Find the record from _messages table
+  // Find the record from  table
     //auto dog_itr = dogtable.find(from.value);
   }
 
@@ -53,11 +110,9 @@ CONTRACT dogs : public contract {
   TABLE dog{
     int id; //unique ID for index
     name owner; //eos account name, owner of dogs
-    std::vector<dogrow> rows; //get and set all data from struct dog.hpp
-
+    std::vector<dogrow> rows; //get and set all data from struct dog_stats.hpp
     //get primary key by ID variable
     uint64_t primary_key() const{return id;}
-
     //get dogs by owner index
     uint64_t by_owner() const{return owner.value;}
   };//end table dog
