06ddda84ef0e4ee64bfba919c88d3bdfb7ab50a8
thecil <53628989+thecil@users.noreply.github.com>
2020-03-27 22:38:45 -0400

    Create dog_struct.hpp

---
+++ EOS/dogcontract/include/dogs.contract/table/dog_struct.hpp
@@ -0,0 +1,5 @@
+//struct dog stats, used to set different statistics for dogs table
+struct dogrow{
+  ds_string dog_img; //used to set an hyperlink to set an image for dog
+  uint8_t age;
+};//end struct dog_stats
